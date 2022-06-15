6f539e1fba551bfb2d6b529c593db64105b94f8a
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2022-06-15 02:50:28 -0700

    created a new tile layer that is collidable - to be placed behind platforms, purple tiles have a boolean collides property in tiled app

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,8 +10,18 @@ class Play extends Phaser.Scene {
         const map = this.make.tilemap({key: 'crystal_world_map'});
         const tileset1 = map.addTilesetImage('crystal_tileset_lev1', 'tiles-1');
 
-        map.createLayer('environment', tileset1);
-        map.createLayer('platforms', tileset1);
+        // creating environment and platform layers from tileset
+        // ORDER OF CODE MATTERS - environment layer is behind platforms layer
+        const platformsColliders = map.createStaticLayer('platforms_colliders', tileset1);
+        const environment = map.createLayer('environment', tileset1);
+        const platforms = map.createLayer('platforms', tileset1);
+
+        // https://phaser.io/examples/v3/view/tilemap/set-colliding-by-property
+        // Instead of setting collision by index, you can set collision via properties that you set up
+        // in Tiled. You can assign properties to tiles in the tileset editor. Note: you need
+        // to NOT have the collision editor or terrain editor open when you set them up.
+        // This map has tiles with a boolean "collides" property, so we can do the following:
+        platformsColliders.setCollisionByProperty({collides: true});
     }
 }
 
