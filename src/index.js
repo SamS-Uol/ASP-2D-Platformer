6256265976e3da3dd9dfdea82c63120cdcad8704
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2022-06-14 17:07:10 -0700

    created a PlayScene class, moved methods to PlayScene, moved assets folder to root folder, loading build file in index.html, rebuilt project
    created a PlayScene class that index.js loads from config,
    
    moved methods from index.js to to PlayScene.js,
    
    moved the assets folder to the root folder from the src folder,
    
    referencing/loading build file in index.html to load game as first thing on webpage when application is loaded,
    
    rebuilt and deployed project - modifies node_modules and dist folder

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
+import PlayScene from './Scenes/PlayScene';
 
 const config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: PlayScene
 };
 
 const game = new Phaser.Game(config);
