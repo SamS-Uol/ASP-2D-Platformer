6fe17a53135979d35faa051f62ecff6f321db359
tzahush <t@bena.co.il>
2022-08-20 13:05:24 +0000

    adding modal

--- website/js/modal.js
+++ website/js/modal.js
@@ -1,4 +1,14 @@
 document.getElementById("play-button").onclick = function (){
     const myModal = new bootstrap.Modal(document.getElementById('gameModal'), {})
+    const modalBody = document.getElementsByClassName("modal-body")[0];
+
     myModal.show();
+    setTimeout(()=>{
+
+        const iframe = document.createElement("iframe");
+        iframe.src = 'https://tzahiuol.github.io/agile-software-projects/game/dist/';
+        iframe.width="100vw";
+        iframe.style.height='100vh';
+        modalBody.appendChild(iframe);
+    },1000)
 }
