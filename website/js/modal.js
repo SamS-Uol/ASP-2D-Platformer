99d21181a4755e8c22541c14d606ddbe2cb48a9c
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2022-08-25 15:11:28 -0700

    testing - linking website directly to dist folder rather than through online link

--- website/js/modal.js
+++ website/js/modal.js
@@ -1,3 +1,5 @@
+import dist from "../../game/dist";
+
 document.getElementById("play-button").onclick = function (){
     const myModal = new bootstrap.Modal(document.getElementById('gameModal'), {})
     const modalBody = document.getElementsByClassName("modal-body")[0];
@@ -6,7 +8,8 @@ document.getElementById("play-button").onclick = function (){
     setTimeout(()=>{
 
         const iframe = document.createElement("iframe");
-        iframe.src = 'https://tzahiuol.github.io/agile-software-projects/game/dist/';
+        // iframe.src = 'https://tzahiuol.github.io/agile-software-projects/game/dist/';
+        iframe.src = dist;
         iframe.width="100vw";
         iframe.style.height='100vh';
         modalBody.appendChild(iframe);
