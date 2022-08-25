245038ea603123c70fd81685c51c2083eb31b8c6
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2022-08-25 16:03:32 -0700

    Editing relative file path of dist folder in modal.js

--- website/js/modal.js
+++ website/js/modal.js
@@ -1,3 +1,5 @@
+// import dist from '../../game/dist';
+
 document.getElementById("play-button").onclick = function (){
     const myModal = new bootstrap.Modal(document.getElementById('gameModal'), {})
     const modalBody = document.getElementsByClassName("modal-body")[0];
@@ -7,7 +9,7 @@ document.getElementById("play-button").onclick = function (){
 
         const iframe = document.createElement("iframe");
         // iframe.src = 'https://tzahiuol.github.io/agile-software-projects/game/dist/';
-        iframe.src = '../../game/dist';
+        iframe.src = '../game/dist';
         iframe.width="100vw";
         iframe.style.height='100vh';
         modalBody.appendChild(iframe);
