aa993c6b90a22ce70e39eb01e29742b7f71b0b42
SamSherrod <107281526+SamSherrod@users.noreply.github.com>
2022-08-25 15:29:46 -0700

    Update modal.js - trying to link dist folder directly to website rather than through online link

--- website/js/modal.js
+++ website/js/modal.js
@@ -1,4 +1,4 @@
-import dist from "../../game/dist";
+// const dist = require("../../game/dist");
 
 document.getElementById("play-button").onclick = function (){
     const myModal = new bootstrap.Modal(document.getElementById('gameModal'), {})
@@ -9,7 +9,7 @@ document.getElementById("play-button").onclick = function (){
 
         const iframe = document.createElement("iframe");
         // iframe.src = 'https://tzahiuol.github.io/agile-software-projects/game/dist/';
-        iframe.src = dist;
+        iframe.src = '../../game/dist';
         iframe.width="100vw";
         iframe.style.height='100vh';
         modalBody.appendChild(iframe);
